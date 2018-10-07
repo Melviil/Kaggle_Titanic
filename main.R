b319901bccbef4d00fcdc22c9854c973782adfdd
Melvil Donnart <melvildonnart3@gmail.com>
2018-10-07 12:19:28 +0200

    Load and check data

--- main.R
+++ main.R
@@ -1,3 +1,9 @@
+install.packages('ggplot2') # visualization
+install.packages('ggthemes') # visualization
+install.packages('scales') # visualization
+install.packages('dplyr') # data manipulation
+install.packages('mice') # imputation
+install.packages('randomForest') # classification algorithm
 # Load packages
 library('ggplot2') # visualization
 library('ggthemes') # visualization
